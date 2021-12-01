5dfdb0f8c4bc1bd307ff3bbe8eee77d103336797
krunalex <krunalex@kkg-do.eu>
2021-12-01 11:56:44 +0100

    Changes by zzxafurk

--- src/main/java/my_project/model/VisualQueue.java
+++ src/main/java/my_project/model/VisualQueue.java
@@ -8,26 +8,86 @@ public class VisualQueue<T extends GraphicalObject & Animatible> {
 
     private final Queue<T> queue;
     private ViewController viewController;
+    private double posX;
+    private double posY;
+    private T previouse;
 
-    public VisualQueue(ViewController viewController){
+    public VisualQueue(ViewController viewController, double posX, double posY){
+        this.posX = posX;
+        this.posY = posY;
+        previouse = null;
         queue = new Queue<>();
         this.viewController = viewController;
     }
 
     public void enqueue(T content){
-        queue.enqueue(content);
-        if(content != null) viewController.draw(content);
+        if(!queue.isEmpty()){
+           queue.enqueue(content);
+           if(content != null) {
+               if(content.getRadius() == 0){
+                   content.setY(posY);
+                   content.setX(posX);
+                   posX += content.getWidth();
+               }else{
+                   posX += content.getRadius();
+                   content.setY(posY);
+                   content.setX(posX);
+                   posX += content.getRadius();
+               }
+           }
+        }else{
+            queue.enqueue(content);
+            content.setY(posY);
+            content.setX(posX);
+            if(content.getRadius() == 0){
+                posX += content.getWidth();
+            }else{
+                posX += content.getRadius();
+            }
+        }
+
+        if(content != null){
+            viewController.draw(content);
+            content.fadeIn();
+        }
     }
 
     public void dequeue(){
         if(!queue.isEmpty()){
             viewController.removeDrawable(queue.front());
             queue.dequeue();
+            if(queue.isEmpty()){
+                posX = 200;
+                posY = 200;
+            }
         }
+
     }
 
     public void front(){
         queue.front();
     }
+
+    //Extra Methoden
+
+    public void fadeIn(){
+        if(queue.isEmpty()){
+            queue.front().fadeIn();
+        }
+    }
+
+    public void fadeOut(){
+        if(queue.isEmpty()){
+            queue.front().fadeOut();
+        }
+    }
+
+    public void changeSize(double size){
+        if(queue.isEmpty()){
+            queue.front().changeSize(size);
+        }
+    }
+
+
 
 }
